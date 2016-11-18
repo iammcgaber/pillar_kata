c3c615a30b44ec957e396f9fb3c7b7664880f665
Gabriel Sheeley <iammcgaber@gmail.com>
2016-11-18 16:34:38 -0500

    Fixed the name of my previous test (doh!) and created a new class for converting Roman Numerals to Arabic numbers.  First test has passed.

---
+++ src/main/java/com/iammcgaber/RomanToArabicNumeralKata.java
@@ -0,0 +1,5 @@
+package com.iammcgaber;
+
+public class RomanToArabicNumeralKata {
+
+}
---
+++ src/test/java/com/iammcgaber/RomanToArabicNumeralKataTest.java
@@ -0,0 +1,30 @@
+package com.iammcgaber;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RomanToArabicNumeralKataTest {
+	RomanToArabicNumeralKata kata;
+	
+	@Before
+	public void create_new_object() {
+		kata = new RomanToArabicNumeralKata();
+	}
+	
+	@Test
+	public void test_that_kata_object_is_not_null() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		Assert.assertNotNull(kata);
+	}
+	
+	
+}
